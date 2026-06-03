5e40d1311036bfa9a696a30cdd26725ff4a44e05
Vicente Alvarez <veeveealvarez94@gmail.com>
2026-06-03 18:00:29 +0200

    A3 part

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :creator
-  has_many :ideas
+  has_many :ideas, dependent: :destroy
+
+  def onboarding_complete?
+    next_onboarding_step == :done
+  end
+
+  def next_onboarding_step
+    return :creator unless creator.present?
+    return :idea    unless ideas.any?
+    return :script  unless Script.where(idea: ideas).exists?
+    return :post    unless LinkedinPost.where(script: Script.where(idea: ideas)).exists?
+
+    :done
+  end
 end
